8fd9830c890ac71c211eb9500d3a2a9f8e18c122
Josua96 <joshuaenrique96@gmail.com>
2018-04-28 22:26:57 -0600

    types and products tables, getproductbyenterprise function

---
+++ SQL Script/script.sql
@@ -0,0 +1,94 @@
+﻿
+/***********************************************************
+TABLES
+************************************************************/
+
+CREATE TABLE productTypes
+(
+   typeID serial NOT NULL UNIQUE,
+   typeName varchar(20) NOT NULL,
+   CONSTRAINT productTypes_PK_typeID PRIMARY KEY (typeID)
+)
+
+
+CREATE TABLE products
+( 
+   productID serial NOT NULL,
+   enterpriseID INTEGER NOT NULL,
+   productName TEXT NOT NULL,
+   code VARCHAR(20) NOT NULL,
+   price INTEGER NOT NULL,
+   unit  BOOLEAN NOT NULL, --true sell in kg, false in bags
+   image TEXT NOT NULL,
+   productType INTEGER NOT NULL,
+   description TEXT NOT NULL,
+   stock INTEGER NOT NULL,
+
+   CONSTRAINT products_PK_productID PRIMARY KEY (productID),
+   CONSTRAINT products_FK_productType FOREIGN KEY (productType) REFERENCES productTypes(typeID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+/*****************************************************************
+STORED PROCDEDURES
+******************************************************************/
+
+/***********************
+For table products
+************************/
+
+-- Get product's information related with an enterprise
+-- Require: enterpriseID
+-- Restrictions: If product's stock is less than 0, product information isn't going to be returned
+CREATE OR REPLACE FUNCTION sp_getProductsByEnterpriseID
+
+(
+	IN i_enterpriseID INTEGER,
+	OUT o_enterpriseID INTEGER,
+	OUT o_productID INTEGER,
+	OUT o_productName TEXT,
+	OUT o_price INTEGER,
+	OUT o_productImage TEXT,
+	OUT o_productUnit BOOLEAN,
+	OUT o_productDescription TEXT,
+	OUT o_productStock INTEGER
+	
+)
+RETURNS
+SETOF RECORD AS
+$body$
+BEGIN
+	RETURN query SELECT p.* FROM
+	(SELECT enterpriseID,productID, productName, price,image,unit,description,stock  FROM products WHERE enterpriseID=i_enterpriseID) AS p 
+	 WHERE p.stock > 0;
+
+END;
+$body$
+LANGUAGE plpgsql;
+
+insert into productTypes(typeName) values ('Frutas')
+select * from productTypes
+delete from productTypes
+
+select * from products
+
+insert into products(
+   enterpriseID ,
+   productName ,
+   code ,
+   price,
+   unit, --true sell in kg, false in bags
+   image,
+   productType,
+   description ,
+   stock
+) values(1,'Manzana','00rt',600,true,'manzanatestimage.png',3,'manzana del atlántico',30),
+  (2,'Manzana2','00rt',600,true,'manzanatestimage.png',3,'manzana del atlántico',0),
+  (3,'Manzana3','00rt',600,true,'manzanatestimage.png',3,'manzana del atlántico',10),
+  (2,'Mango','00rt',600,true,'manzanatestimage.png',3,'mango del atlántico',0),
+  (2,'Naranja','00rt',600,true,'manzanatestimage.png',3,'Naranja del atlántico',30),
+  (3,'Piña','00rt',600,true,'manzanatestimage.png',3,'Piña del atlántico',10);
+
+select sp_getProductsByEnterpriseID(3);
