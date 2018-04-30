3f4e68035047bd7cafb39b90f3de3e8bbf9dba20
Nathalie Rojas <nathalieroarce08@gmail.com>
2018-04-30 13:23:40 -0600

    commit

--- Service/App.js
+++ Service/App.js
@@ -9,4 +9,21 @@ var app = express()
  //  .get('/', (req, res) => res.render('service/index'))
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+const { BD } = require('pg');
+const bd = new BD({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
+app.get('/sp_getProductsByEnterpriseID', async (req, res) => {
+    try {
+        const client = await bd.connect()
+        const result = await client.query('select p_getProductsByEnterpriseID(1)');
+      //  res.render('View/example', result);
+        return result;
+        //client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
