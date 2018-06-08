15087c133adf947320d2594e3594a79d6205ad91
nathalieroarce08 <nathalieroarce08@gmail.com>
2018-06-08 11:22:49 -0600

    modal editar productos

--- webApp/src/app/administrator-view/orders-view/orders-view.component.ts
+++ webApp/src/app/administrator-view/orders-view/orders-view.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { OrdersService } from '../../services/administrador-view/orders.service';
 import { userNotifications } from '../../models/userNotifications';
-
 declare var swal:any;
 
 
@@ -12,39 +11,43 @@ declare var swal:any;
 })
 export class OrdersViewComponent implements OnInit {
 
-  /**
+   /**
    * typeOfAction, false= cancel order ; true= send order
    */
   private typeOfAction: Boolean;
-  private loadingOrders:Boolean;
+  private loadingOrders: Boolean;
   private filters: Array<any>;
   private currentItem: boolean;
   private enterpriseID: Number;
   private clientDeliveryDate: Date;
-  private justification:String;
+  private justification: String;
   private orderID: Number;
   private userNotify: userNotifications;
+  private productsOrder : any;
 
   constructor(private orders : OrdersService) {
-    this.userNotify= new userNotifications(); 
+    this.userNotify= new userNotifications();
     this.filters= [{"text":"Pedidos no atendidos","value": false},{"text":"Pedidos enviados","value": true}]
     this.currentItem= this.filters[0].value;
     this.enterpriseID=1;
     this.loadOrders();
-    
-  
   }
 
   public loadOrders(){
-    
+
     if (this.currentItem== true){
       this.orders.getOrders("getSentOrders",this.enterpriseID).then();
-      
+
     }
 
     else{
       this.orders.getOrders("getPendingOrders",this.enterpriseID).then();
     }
+  }
+
+  public showProducts( order : any){
+    this.orders.
+
   }
 
   public setAction(orderId:Number,action:Boolean){
@@ -53,15 +56,15 @@ export class OrdersViewComponent implements OnInit {
   }
 
   public sendOrder(){
-   
+
     if (this.clientDeliveryDate ===undefined){
-      this.userNotify.notify(1,"No se ha especificado la fecha en la que el pedido será entregado", 
+      this.userNotify.notify(1,"No se ha especificado la fecha en la que el pedido será entregado",
       "Notificación del sistema");
     }
     else{
 
       this.orders.sendOrder(this.orderID,this.clientDeliveryDate).then (response => {
-        
+
         if (response===false){
           this.userNotify.notify(1,"No ha sido posible marcar el pedido como enviado", "Notificación del sistema");
         }
@@ -75,7 +78,7 @@ export class OrdersViewComponent implements OnInit {
   public cancelEnterpriseOrder(){
 
     if (this.justification ===undefined || this.justification.length===0){
-      this.userNotify.notify(1,"No se ha especificado la razón por la cuál se rechazará el pedido", 
+      this.userNotify.notify(1,"No se ha especificado la razón por la cuál se rechazará el pedido",
       "Notificación del sistema");
     }
     else{
